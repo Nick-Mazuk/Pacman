e480a959d50ab68416a02292e6350b156a7bd115
Nick-Mazuk <trainridernick@gmail.com>
2014-06-10 16:25:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ function onload() {
                 requestAnimationFrame(ghostimgAnimations);
                 
                 if (frame%2) {
-                        document.getElementsByName('gLr1').src = redGhostImages[0];
+                        document.getElementsByName('gLr1').src = "Pictures/g12.png";
                         document.getElementById("gLr2").innerHTML = '<img name="g_i1" src="Pictures/g22.png">';
                         document.getElementById("gLr3").innerHTML = '<img name="g_i1" src="Pictures/g32.png">';
                         document.getElementById("gLr4").innerHTML = '<img name="g_i1" src="Pictures/g42.png">';
