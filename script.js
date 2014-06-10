10291e4c1ef992e661584038871ba1f3e0e55713
Nick-Mazuk <trainridernick@gmail.com>
2014-06-10 15:43:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
-//
+function onLoad() {
+
+        // pre-loading
+
+        function preLoad() {
+                
+                //Red Ghost Images
+                var g11 = new Image();
+                g11.src='Pictures/g11.png';
+                var g12 = new Image();
+                g12.src='Pictures/g12.png';
+                var redGhostImages = [g11,g12];
+                
+                //Blue Ghost Images
+                var g21 = new Image();
+                g21.src='Pictures/g21.png';
+                var g22 = new Image();
+                g22.src='Pictures/g22.png';
+                var redGhostImages = [g21,g22];
+                
+                //Orange Ghost Images
+                var g31 = new Image();
+                g31.src='Pictures/g31.png';
+                var g32 = new Image();
+                g32.src='Pictures/g32.png';
+                var redGhostImages = [g31,g32];
+                
+                //Pink Ghost Images
+                var g41 = new Image();
+                g41.src='Pictures/g41.png';
+                var g42 = new Image();
+                g42.src='Pictures/g42.png';
+                var redGhostImages = [g41,g42];
+                
+                //Pacman Left Images
+                var img1 = new Image();
+                img1.src='Pictures/l1.png';
+                var img2 = new Image();
+                img2.src='Pictures/l2.png';
+                var img3 = new Image();
+                img3.src='Pictures/l3.png';
+                var left = [img2,img3,img2,img1];
+                
+                //Pacman Right Images
+                var img1 = new Image();
+                img1.src='Pictures/r1.png';
+                var img2 = new Image();
+                img2.src='Pictures/r2.png';
+                var img3 = new Image();
+                img3.src='Pictures/r3.png';
+                var right = [img2,img3,img2,img1];
+                
+                //Pacman Down Images
+                var img1 = new Image();
+                img1.src='Pictures/d1.png';
+                var img2 = new Image();
+                img2.src='Pictures/d2.png';
+                var img3 = new Image();
+                img3.src='Pictures/d3.png';
+                var down = [img2,img3,img2,img1];
+                
+                //Pacman Up Images
+                var img1 = new Image();
+                img1.src='Pictures/u1.png';
+                var img2 = new Image();
+                img2.src='Pictures/u2.png';
+                var img3 = new Image();
+                img3.src='Pictures/u3.png';
+                var up = [img2,img3,img2,img1];
+                
+                var PacImg = [left,right,down,up]
+        }
+
+        // load only, if images are supported
+        if (document.images) preLoad();
+        
+}
