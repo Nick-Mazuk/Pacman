cb4c7997e31349b16319cbd783188bc3ae391784
Nick-Mazuk <trainridernick@gmail.com>
2014-06-10 15:58:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var redGhostImages = [];
+var blueGhostImages = [];
+var orangeGhostImages = [];
+var pinkGhostImages = [];
+var PacImg = [];
+
 function onLoad() {
 
         // pre-loading
@@ -9,28 +15,32 @@ function onLoad() {
                 g11.src='Pictures/g11.png';
                 var g12 = new Image();
                 g12.src='Pictures/g12.png';
-                var redGhostImages = [g11,g12];
+                redGhostImages[0] = g11;
+                redGhostImages[1] = g12;
                 
                 //Blue Ghost Images
                 var g21 = new Image();
                 g21.src='Pictures/g21.png';
                 var g22 = new Image();
                 g22.src='Pictures/g22.png';
-                var redGhostImages = [g21,g22];
+                blueGhostImages[0] = g21;
+                blueGhostImages[1] = g22;
                 
                 //Orange Ghost Images
                 var g31 = new Image();
                 g31.src='Pictures/g31.png';
                 var g32 = new Image();
                 g32.src='Pictures/g32.png';
-                var redGhostImages = [g31,g32];
+                orangeGhostImages[0] = g31;
+                orangeGhostImages[1] = g32;
                 
                 //Pink Ghost Images
                 var g41 = new Image();
                 g41.src='Pictures/g41.png';
                 var g42 = new Image();
                 g42.src='Pictures/g42.png';
-                var redGhostImages = [g41,g42];
+                pinkGhostImages[0] = g41;
+                pinkGhostImages[1] = g42;
                 
                 //Pacman Left Images
                 var img1 = new Image();
@@ -68,7 +78,10 @@ function onLoad() {
                 img3.src='Pictures/u3.png';
                 var up = [img2,img3,img2,img1];
                 
-                var PacImg = [left,right,down,up]
+                PacImg[0] = img2;
+                PacImg[1] = img3;
+                PacImg[2] = img2;
+                PacImg[3] = img2;
         }
 
         // load only, if images are supported
